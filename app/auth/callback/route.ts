79320416c9e7cacce88d7ee130a2aea49910c8b5
Harry <hary050506@gmail.com>
2026-05-28 13:54:38 +0700

    Switch to createServerSupabaseClient for auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,11 +1,11 @@
-import { createClient } from '@/lib/supabase'
+import { createServerSupabaseClient } from '@/lib/supabase-server'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   if (code) {
-    const supabase = createClient()
+    const supabase = createServerSupabaseClient()
     await supabase.auth.exchangeCodeForSession(code)
   }
   return NextResponse.redirect(`${origin}/`)
